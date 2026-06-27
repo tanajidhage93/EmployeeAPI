a0861ebb7671761729d99da423870c9ae7218480
tanajidhage93 <tanajidhage93@gmail.com>
2026-06-27 16:48:24 +0530

    Delta Controller added.

---
+++ EmployeeDAPI/Controllers/DeltaController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeDAPI.Controllers
+{
+    public class DeltaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Display()
+        {
+            return Ok("Successfull!");
+        }
+    }
+}
